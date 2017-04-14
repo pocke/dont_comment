da1cea18b7fcd3225c53d304645f4c7a1e6472a9
Masataka Kuwabara <kuwabara@pocke.me>
2017-04-14 19:27:08 +0900

    [Fix #5] Add `--formatter` option

--- lib/dont_comment.rb
+++ lib/dont_comment.rb
@@ -1,10 +1,15 @@
 require 'parser/current'
-require 'stringio'
 
-require 'dont_comment/cli'
+require 'stringio'
+require 'optparse'
+require 'json'
+require 'pathname'
+
 require 'dont_comment/formatter'
 require 'dont_comment/formatter/base'
 require 'dont_comment/formatter/simple'
+require 'dont_comment/formatter/json'
+require 'dont_comment/cli'
 require 'dont_comment/offense'
 require 'dont_comment/runner'
 require "dont_comment/version"
--- lib/dont_comment/formatter/simple.rb
+++ lib/dont_comment/formatter/simple.rb
@@ -1,10 +1,6 @@
 module DontComment
   module Formatter
     class Simple < Base
-      def initialize(offenses)
-        @offenses = offenses
-      end
-
       def format(io)
         @offenses.each do |offense|
           io.puts "#{offense.relative_path}:#{offense.loc.line}: Do not comment out Ruby code"
