60724d3f6040426b23b3fe50639b23cfaddc6942
Masataka Kuwabara <kuwabara@pocke.me>
2017-04-14 12:06:17 +0900

    Join comments

--- dont_comment.gemspec
+++ dont_comment.gemspec
@@ -20,6 +20,8 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
+  spec.required_ruby_version = '>= 2.4'
+
   spec.add_dependency 'parser'
 
   spec.add_development_dependency "bundler", "~> 1.14"
--- lib/dont_comment.rb
+++ lib/dont_comment.rb
@@ -4,9 +4,11 @@ require 'parser/current'
 module DontComment
   def self.execute(argv)
     files = argv
-    files.each do |file|
+    files.each do |fname|
       parser = Parser::CurrentRuby.new
-      _ast, comment, _tokens = parser.tokenize(buffer(file))
+      buffer = buffer(fname)
+      locs = comment_locations(parser, buffer)
+      p locs
     end
   end
 
@@ -14,5 +16,46 @@ module DontComment
     Parser::Source::Buffer.new(fname, 1).tap do |buffer|
       buffer.source = File.read(fname)
     end
+  end
+
+  # @example
+  # One comment
+  #   # foo
+  #   # bar
+  #   def foo; end
+  #
+  # One comment
+  #   # foo
+  #   def foo; end
+  #
+  # Two comments
+  #   # foo
+  #   def foo; end
+  #   # bar
+  def self.comment_locations(parser, buffer)
+    _ast, comments, _tokens = parser.tokenize(buffer)
+
+    comments.each_with_object([]) do |comment, locs|
+      prev = locs.last
+      cur = comment.loc.expression
+
+      if prev && continuous_comment?(prev, cur)
+        locs[locs.size-1] = prev.join(cur)
+      else
+        locs.push(cur)
+      end
+    end.freeze
+  end
+
+  # @param prev [Parser::Source::Range]
+  # @param cur [Parser::Source::Range]
+  def self.continuous_comment?(prev, cur)
+    range_between(prev, cur).source.match?(/\A\s*\z/)
+  end
+
+  # @param prev [Parser::Source::Range]
+  # @param cur [Parser::Source::Range]
+  def self.range_between(prev, cur)
+    Parser::Source::Range.new(prev.source_buffer, prev.end_pos, cur.begin_pos)
   end
 end
