fc13fb8d5e534a1c200cce37252cc3b96989245e
Masataka Kuwabara <kuwabara@pocke.me>
2017-04-15 13:35:42 +0900

    Add test for --json option

--- test/test_integration.rb
+++ test/test_integration.rb
@@ -14,4 +14,22 @@ class TestIntegration < Minitest::Test
     assert { lines[0].chomp == 'test/data/sample.rb:3: Do not comment out unused code, use version control system instead and remove it!'}
     assert { lines[1].chomp == 'test/data/sample.rb:7: Do not comment out unused code, use version control system instead and remove it!'}
   end
+
+  def test_json_option
+    exe = Pathname(__dir__).join('../exe/dont_comment').to_s
+    rb = Pathname(__dir__).join('data/sample.rb').to_s
+    stdout, _stderr, status = Open3.capture3('ruby', exe, rb, '--format', 'json')
+    assert { status.success? }
+
+    result = JSON.parse(stdout)
+    assert { result.is_a? Array }
+    result.each do |offense|
+      assert { offense['path'] == 'test/data/sample.rb' }
+    end
+
+    assert { result[1]['location']['start_line'] == 7 }
+    assert { result[1]['location']['end_line'] == 9 }
+    assert { result[1]['location']['start_column'] == 0 }
+    assert { result[1]['location']['end_column'] == 5 }
+  end
 end
